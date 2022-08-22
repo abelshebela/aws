c453ea55feef74fdeece746beddc169b6fcff191
avel <avelabebe@gmail.com>
2022-08-22 13:05:31 +0300

    First Commit

---
+++ forms/login.php
@@ -0,0 +1,91 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Muhamad Nauval Azhar">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="This is a login page template based on Bootstrap 5">
+	<title>Login Page</title>
+
+	 <!-- Favicons -->
+	 <link href="../assets/img/favicon.png" rel="icon">
+	 <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+   
+	 <!-- Google Fonts -->
+	 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+   
+	 <!-- Vendor CSS Files -->
+	 <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+	 <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	 <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	 <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	 <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+	 <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+	 <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+
+<body>
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-sm-center h-100">
+				<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+					
+					<div class="text-center my-10">
+					<a href="../index.php"><i class="bi bi-arrow-return-left"></i></a>
+						<img src="../assets/img/aws_logo(2).png" alt="logo" width="100">
+					</div>
+					<div class="card shadow-lg">
+						<div class="card-body p-5">
+							<h1 class="fs-4 card-title fw-bold mb-4">Login</h1>
+							<form method="POST" class="needs-validation" novalidate="" autocomplete="off">
+								<div class="mb-3">
+									<label class="mb-2 text-muted" for="email">E-Mail Address</label>
+									<input id="email" type="email" class="form-control" name="email" value="" required autofocus>
+									<div class="invalid-feedback">
+										Email is invalid
+									</div>
+								</div>
+
+								<div class="mb-3">
+									<div class="mb-2 w-100">
+										<label class="text-muted" for="password">Password</label>
+										<a href="forgot.html" class="float-end">
+											Forgot Password?
+										</a>
+									</div>
+									<input id="password" type="password" class="form-control" name="password" required>
+								    <div class="invalid-feedback">
+								    	Password is required
+							    	</div>
+								</div>
+
+								<div class="d-flex align-items-center">
+									<div class="form-check">
+										<input type="checkbox" name="remember" id="remember" class="form-check-input">
+										<label for="remember" class="form-check-label">Remember Me</label>
+									</div>
+									<button type="submit" class="btn btn-primary ms-auto">
+										Login
+									</button>
+								</div>
+							</form>
+						</div>
+						<div class="card-footer py-3 border-0">
+							<div class="text-center">
+								Don't have an account? <a href="register.php" class="text-dark">Create One</a>
+							</div>
+						</div>
+					</div>
+					<div class="text-center mt-5 text-muted">
+						Copyright &copy; 2022 &mdash; AWS Cloud Practitioner Traniees. 
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/login.js"></script>
+</body>
+</html>
